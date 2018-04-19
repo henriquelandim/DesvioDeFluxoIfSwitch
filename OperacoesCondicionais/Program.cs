3a76375ee88e17a2918f679fdb63905c598cc0c1
Henrique Landim <henriquelandim@outlook.com.br>
2018-04-19 11:11:03 -0300

    Declaração das variáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,15 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  comentário in-line(em linha)
+            /*
+                comentário in-block(em bloco)
+            */
+            //declaração da variável opereação. Ela será do tipo inteira
+            int opereacao;
+            //Declaração das variáveis n1 e n2. Elas serão do tipo DOUBLE
+            double n1, n2;
+
         }
     }
 }
